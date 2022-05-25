670746d843fa653946485732089d4c812a3835de
Hyusicul <105685800+Hyusicul@users.noreply.github.com>
2022-05-25 21:08:12 +0900

    Add files via upload

---
+++ w1.py
@@ -0,0 +1,33 @@
+# A=(2.5*3)/27
+# B=(4*2)/30
+# C=A>B
+# # # print(C)
+#
+# S=45
+# C=30
+# V=15
+# r=S/(C+V)
+# print(r)
+
+
+# bread_cal = 223.5
+#
+# print('빵을 몇 개 먹었습니까?')
+# eatbread = int(input())
+#
+# eatcal = bread_cal * eatbread
+#
+# print('당신은', eatcal , '만큼의 칼로리를 섭취하셨네요.')
+
+print('첫 번째 정수를 입력하세요')
+
+first_number = int(input())
+
+print('두 번째 정수를 입력하세요')
+
+second_number = int(input())
+
+print(first_number , '+' , second_number , '=' , first_number+second_number)
+print(first_number , '-' , second_number , '=' , first_number-second_number)
+print(first_number , '*' , second_number , '=' , first_number*second_number)
+print(first_number , '/' , second_number , '=' , first_number/second_number)
